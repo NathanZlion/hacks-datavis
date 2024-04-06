12a41d3ce789e64b5eaf5046178cb9d14c070c07
Nathnael Dereje <nathandere1357@gmail.com>
2024-04-06 12:43:54 +0300

    Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Hero } from './components/Hero/hero.component'
+import { Countries } from './components/Countries/countries.component'
+import { HeardAboutUs } from './components/Hear-About-Us/hear.component'
+import { Navbar } from './components/Navbar/navbar.component'
+import { Footer } from './components/footer/footer.component'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='relative bg-black text-white'>
+      <Navbar />
+      <div className='flex flex-col gap-5 bg-red-300	'>
+        <Hero />
+        <Countries />
+        <HeardAboutUs />
       </div>
-      <h1 className='text-dark'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs text-3xl">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Footer />
+    </div>
   )
 }
 
-export default App
+export default App;
