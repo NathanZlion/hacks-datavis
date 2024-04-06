973b4402ebe5ee903699205de55d6725e82236a4
Nathnael Dereje <nathandere1357@gmail.com>
2024-04-06 12:41:37 +0300

    Add footer component

---
+++ src/components/footer/footer.component.tsx
@@ -0,0 +1,10 @@
+
+export const Footer = () => {
+    return (
+        <footer className="bg-gray-900 text-white text-center m-0">
+            <div className="container mx-auto">
+                <p>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+}
