4dcfd0da6ae02e96bd04cb8ac68dd763056fa4b4
Nathnael Dereje <nathandere1357@gmail.com>
2024-06-10 03:46:41 +0300

    Remove dummy data and use state

--- src/components/barchart/barchart.component.tsx
+++ src/components/barchart/barchart.component.tsx
@@ -2,35 +2,23 @@
 
 import Chart from "react-google-charts"
 import './barchart.css';
+import { countryData } from "../../state/countries.slice";
+import { useSelector } from "react-redux";
+import { grandStateEnum } from "../../state/grandstate.slice";
 
-const INNERHEIGHT = 50; //pixels
+const INNERHEIGHT = 400; //pixels
 
 interface props {
     title: string
+    individual?: boolean
 }
 
-export const BarChart = ({ title }: props) => {
-    const data = [
-        [title, "Number of Participants"],
-        ["South Africa", 46],
-        ["Somalia", 18],
-        ["Ethiopia", 23],
-        ["Djibouti", 12],
-        ["Somalia", 18],
-        ["Ethiopia", 23],
-        ["Djibouti", 12],
-        ["Somalia", 18],
-        ["Ethiopia", 23],
-        ["Djibouti", 12],
-        ["Somalia", 18],
-        ["Ethiopia", 23],
-        ["Djibouti", 12],
-        ["Somalia", 18],
-        ["Ethiopia", 23],
-        ["Djibouti", 12],
-        ["Algeria", 38],
-    ];  
-    var chartAreaHeight = (data.length - 1) * INNERHEIGHT;
+export const BarChart = ({ title, individual }: props) => {
+    const countriesData: countryData = useSelector((state: any) => state.countries.value.payload);
+    const countriesDataArray = Object.entries(countriesData).map(([key, value]) => ({ key, value }));
+    var entriesLength = countriesData ? countriesDataArray.length - 1 : 5;
+    
+    var chartAreaHeight = entriesLength * INNERHEIGHT;
     
     const options = {
         title: title,
@@ -38,31 +26,35 @@ export const BarChart = ({ title }: props) => {
         titleTextStyle: { color: '#FFF' },
 
         hAxis: {
-          textStyle: { color: '#FFF' },
+            textStyle: { color: '#FFF' },
             minValue: 0,
         },
         vAxis: {
             title: "Countries",
         },
-      // Colors only the chart area, with opacity
-      left: 10,
-      top: 10,
-      chartArea: {
-        // width: '100%',
-      },
-      height: chartAreaHeight,
-      legend: { position: "none" },
-      backgroundColor: "transparent",
+        // Colors only the chart area, with opacity
+        left: 10,
+        top: 10,
+        chartArea: {
+            // width: '100%',
+        },
+        height: 500,
+        legend: { position: "none" },
+        backgroundColor: "transparent",
     };
-
+    
     return (
         <div className="chart-wrapper">
             < Chart
-                // graph_id="#detailedCharts"
-                className ="barchart"
+                graph_id="#detailedCharts"
+                className="barchart"
                 chartType="BarChart"
                 width="100%"
-                data={data}
+                data={[
+                    ["Country", title],
+                    // @ts-ignore
+                    ...Object.entries(countriesData).map(([key, value]) => [key, individual ? value.numberOfIndividualParticipants : value.numberOfGroupParticipants])
+                ]}
                 options={options}
             />
         </div>
