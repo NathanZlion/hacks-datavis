8709198c59b4465a60c2ded68a6b05ed25ac8e33
Nathnael Dereje <nathandere1357@gmail.com>
2024-06-10 03:48:19 +0300

    Add summary title
    Add a title for the summary that shows what it is

--- src/components/count/count.component.tsx
+++ src/components/count/count.component.tsx
@@ -5,22 +5,27 @@ export const Count = () => {
     const people = useSelector((state: any ) => state.people.value);
 
     return (
-        <div className="count-wrapper">
-            <div className="count-card">
-                <div className="count-card-number">{people!.totalParticipants}</div>
-                <div className="count-card-description">Students</div>
+        <div>
+            <div className="title text-center">
+                Summary Stat
             </div>
-            <div className="count-card">
-                <div className="count-card-number">{people!.individualParticipants}</div>
-                <div className="count-card-description">Individials</div>
-            </div>
-            <div className="count-card">
-                <div className="count-card-number">{people!.groupParticipants}</div>
-                <div className="count-card-description">Groups</div>
-            </div>
-            <div className="count-card">
-                <div className="count-card-number">{people!.totalCountries}</div>
-                <div className="count-card-description">Countries</div>
+            <div className="count-wrapper">
+                <div className="count-card">
+                    <div className="count-card-number">{people!.totalParticipants}</div>
+                    <div className="count-card-description">Students</div>
+                </div>
+                <div className="count-card">
+                    <div className="count-card-number">{people!.individualParticipants}</div>
+                    <div className="count-card-description">Individials</div>
+                </div>
+                <div className="count-card">
+                    <div className="count-card-number">{people!.groupParticipants}</div>
+                    <div className="count-card-description">Groups</div>
+                </div>
+                <div className="count-card">
+                    <div className="count-card-number">{people!.totalCountries}</div>
+                    <div className="count-card-description">Countries</div>
+                </div>
             </div>
         </div>
     );
