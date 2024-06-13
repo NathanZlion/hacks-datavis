2b3048e9dda7af931a295aecd36b8454cbb19bf6
Nathnael Dereje <nathandere1357@gmail.com>
2024-06-13 20:59:09 +0300

    Modify hero section numbers into good-looking cards

--- src/components/count/count.component.tsx
+++ src/components/count/count.component.tsx
@@ -1,31 +1,25 @@
 import { useSelector } from "react-redux";
 import './count.css'
+import { Card } from "../summary card/card.component";
+import { faClipboardUser, faUsersLine } from '@fortawesome/free-solid-svg-icons'
+import { faPerson } from "@fortawesome/free-solid-svg-icons/faPerson";
+import {faEarthAfrica} from '@fortawesome/free-solid-svg-icons/faEarthAfrica';
+
 
 export const Count = () => {
     const people = useSelector((state: any ) => state.people.value);
 
     return (
-        <div>
+        <div className="">
             <div className="title text-center">
                 Summary Stat
             </div>
-            <div className="count-wrapper">
-                <div className="count-card">
-                    <div className="count-card-number">{people!.totalParticipants}</div>
-                    <div className="count-card-description">Students</div>
-                </div>
-                <div className="count-card">
-                    <div className="count-card-number">{people!.individualParticipants}</div>
-                    <div className="count-card-description">Individials</div>
-                </div>
-                <div className="count-card">
-                    <div className="count-card-number">{people!.groupParticipants}</div>
-                    <div className="count-card-description">Groups</div>
-                </div>
-                <div className="count-card">
-                    <div className="count-card-number">{people!.totalCountries}</div>
-                    <div className="count-card-description">Countries</div>
-                </div>
+
+            <div className="count-wrapper m-auto w-4/5 px-auto">
+                <Card title="Total Registration" icon={faClipboardUser} value={people ? people.totalParticipants : 0} />
+                <Card title="Countries" icon={faEarthAfrica} value={people ? people.countryCount : 0} />
+                <Card title="Individuals" icon={faPerson} value={people ? people.individualParticipants : 0} />
+                <Card title="Groups" icon={faUsersLine} value={people ? people.groupParticipants : 0} />
             </div>
         </div>
     );
