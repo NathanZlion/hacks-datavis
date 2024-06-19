3ae83191fad5fb3433ce18b1c715dc03a8a8266a
Nathnael Dereje <nathandere1357@gmail.com>
2024-06-19 06:39:51 +0300

    Make cards mobile responsive

--- src/components/count/count.component.tsx
+++ src/components/count/count.component.tsx
@@ -16,10 +16,10 @@ export const Count = () => {
             </div>
 
             <div className="count-wrapper m-auto w-4/5 px-auto">
-                <Card title="Total Registration" icon={faClipboardUser} value={people ? people.totalParticipants : 0} />
-                <Card title="Countries" icon={faEarthAfrica} value={people ? people.countryCount : 0} />
-                <Card title="Individuals" icon={faPerson} value={people ? people.individualParticipants : 0} />
-                <Card title="Groups" icon={faUsersLine} value={people ? people.groupParticipants : 0} />
+                <Card className="w-full h-[150px] md:h-auto" title="Total Registration" icon={faClipboardUser} value={people ? people.totalParticipants : 0} />
+                <Card className="w-full h-[150px] md:h-auto" title="Countries" icon={faEarthAfrica} value={people ? people.countryCount : 0} />
+                <Card className="w-full h-[150px] md:h-auto" title="Individuals" icon={faPerson} value={people ? people.individualParticipants : 0} />
+                <Card className="w-full h-[150px] md:h-auto" title="Groups" icon={faUsersLine} value={people ? people.groupParticipants : 0} />
             </div>
         </div>
     );
