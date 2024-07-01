322ac5567498a5730584d0ced340d46fca9d48be
Nathnael Dereje <nathandere1357@gmail.com>
2024-07-01 12:21:07 +0300

    Fix bug: On Theme selection button

--- src/components/ui/modetoggle.tsx
+++ src/components/ui/modetoggle.tsx
@@ -25,9 +25,9 @@ export function ModeToggle() {
         <Tooltip>
           <TooltipTrigger>
             <DropdownMenuTrigger asChild>
-              <div>
-                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <div className="w-8 h-8">
+                <Moon className="h-0 w-0 dark:h-full dark:w-full rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <Sun className="h-full w-full dark:h-0 dark:w-0 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                 <span className="sr-only">Toggle theme</span>
               </div>
             </DropdownMenuTrigger>
