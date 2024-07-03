6caa9f70a8aded5c2bab1cd579f7ea396b976f72
Nathnael Dereje <nathandere1357@gmail.com>
2024-07-03 12:06:45 +0300

    Clean up - percomponent remove css files to keep code clean
    It becomes difficult to manage code, to know where the styles are coming from. So the css files are removed and the rules written there are added to the components as tailwind classes.

--- src/components/Hero/hero.component.tsx
+++ src/components/Hero/hero.component.tsx
@@ -1,24 +1,23 @@
 import { Count } from '../Count/count.component';
-import './hero.css';
 import { HorizontalLine } from '../ui/divider';
 import { ArrowBigDown } from 'lucide-react';
 
 export const Hero = () => {
     return (
-        <div className='hero-wrapper'>
+        <div className='min-h-[calc(100vh-20px)] md:p-10 pt-40 mt-9 flex justify-between flex-col'>
             <div className="md:mt-20">
                 <Count />
             </div>
 
-            <HorizontalLine />
 
             {/* a stylish arrow down */}
             <a href="#country_distribution" className='w-16 h-16 mx-auto dark:bg-slate-800 rounded-full flex align-middle justify-center hover:shadow-lg hover:shadow-white hover:animate-pulse'>
                 <button>
-                    <ArrowBigDown className='text-primary dark:text-white'/>
+                    <ArrowBigDown className='text-primary dark:text-white' />
                 </button>
             </a>
-            
+
+            <HorizontalLine />
         </div>
     );
 }
