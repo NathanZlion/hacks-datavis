672108f2bc96c744e6a95121befff42fad241600
Nathnael Dereje <nathandere1357@gmail.com>
2024-07-11 16:28:39 +0300

    Remove developer name

--- src/components/Footer/footer.component.tsx
+++ src/components/Footer/footer.component.tsx
@@ -5,8 +5,7 @@ export const Footer = () => {
         <footer className="flex flex-col lg:flex-row align-center justify-center text-center m-0 bg-secondary dark:text-white p-2 px-5">
             <a href="https://github.com/NathanZlion/hacks-datavis"
                 target="_blank"
-                className="flex flex-1 text-primary align-center w-fill gap-2 justify-center">
-                <span className="text-secondary-foreground"> NattyD. </span>
+                className="flex flex-1 text-primary align-center w-fill gap-2 justify-center order-2">
                 <IoLogoGithub className="text-accent-foreground animate animate-pulse h-full w-5 " />
             </a>
             <div className="container mx-auto flex-auto content-start align-center">
