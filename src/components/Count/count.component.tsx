542408582f99125f00e72f00c271e5bd9a7fc5de
Nathnael Dereje <nathandere1357@gmail.com>
2024-07-15 15:36:37 +0300

    Fix naming summary => participation

--- src/components/Count/count.component.tsx
+++ src/components/Count/count.component.tsx
@@ -12,9 +12,9 @@ export const Count = () => {
         <div className="flex gap-6 flex-col">
             <div className="text-4xl text-center"> A2SV AI for Impact Hackathon </div>
 
-            <div className="text-center text-2xl"> Summary Stat </div>
+            <div className="text-center text-2xl"> Participation Statistics </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-start md:justify-center gap-3 text-center  mx-auto m-auto w-full px-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 justify-start md:justify-center gap-3 text-center  mx-auto m-auto w-full px-auto">
                 {/* Total participation */}
                 <CountCard cardTitle={"Total Registration"} cardValue={people ? people.totalParticipants : 0} cardIcon={faClipboardUser} />
 
