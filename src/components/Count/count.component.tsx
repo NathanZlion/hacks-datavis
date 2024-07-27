0c913de037336a5485768b98299af8719ecc490d
Nathnael Dereje <nathandere1357@gmail.com>
2024-07-27 03:34:09 +0300

    Add project ideas submitted without registration into number

--- src/components/Count/count.component.tsx
+++ src/components/Count/count.component.tsx
@@ -15,8 +15,8 @@ export const Count = () => {
             <div className="text-center text-2xl"> Participation Statistics </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 justify-start md:justify-center gap-3 text-center  mx-auto m-auto w-full px-auto">
-                {/* Total participation */}
-                <CountCard cardTitle={"Total Registration"} cardValue={people ? people.totalParticipants : 0} cardIcon={faClipboardUser} />
+                {/* Total registration plus, idea submission without registering */}
+                <CountCard cardTitle={"Total Registration"} cardValue={people ? people.totalParticipants + 122 : 0} cardIcon={faClipboardUser} />
 
                 {/* Countries count */}
                 <CountCard cardTitle={"Countries"} cardValue={people ? people.countryCount : 0} cardIcon={faEarthAfrica}/>
@@ -24,8 +24,8 @@ export const Count = () => {
                 {/* Individual participation */}
                 <CountCard cardTitle={"Individuals"} cardValue={people ? people.individualParticipants : 0} cardIcon={faPerson} />
 
-                {/* Group participation */}
-                <CountCard cardTitle={"Groups"} cardValue={people ? people.groupParticipants : 0} cardIcon={faUsersLine} />
+                {/* Group registrations plus, idea submission without registering */}
+                <CountCard cardTitle={"Groups"} cardValue={people ? people.groupParticipants + 36: 0} cardIcon={faUsersLine} />
             </div>
         </div>
     );
