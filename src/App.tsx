d5c160c84286459c777f9e59487f387f81d5caf1
Nathnael Dereje <nathandere1357@gmail.com>
2024-08-09 15:26:12 +0300

    Add layout for overview, semifinals, and quarterfinals as tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { Hero } from './components/Hero/hero.component'
-import { Countries } from './components/Countries/countries.component'
-import { HeardAboutUs } from './components/Hear-About-Us/hear.component'
 import { Navbar } from './components/Navbar/navbar.component'
 import { Footer } from './components/Footer/footer.component'
-import { PrevHackathonPartcipation } from './components/PrevHackathonParticipation/prevHackathonParticipation.component'
 import { Toaster } from '@/components/ui/toaster'
 import { ThemeProvider } from "@/services/themeProvider";
 import { Analytics } from "@vercel/analytics/react"
+import {
+  Tab,
+  TabGroup,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from '@tremor/react';
+import Overview from './pages/overview';
+import Quarterfinals from './pages/quarterfinals';
+import Semifinals from './pages/semifinals';
 
 function App() {
-
 
   return (
     <ThemeProvider
@@ -19,17 +24,33 @@ function App() {
     disableTransitionOnChange
     >
       <Analytics />
+      {/* the navbar goes here */}
       {/* {children} */}
-      <div className='relative bg-background text-primary'>
+      <div className='relative bg-background text-primary flex flex-col'>
+
+        {/* the whole things below will be an outlet */}
         <Navbar />
-        <div className='flex flex-col gap-0'>
-          <Hero />
-          <Countries />
-          <div className="flex flex-col lg:flex-row">
-          <HeardAboutUs />
-          <PrevHackathonPartcipation />
-          </div>
-        </div>
+        <TabGroup className='px-5 flex-1'>
+          <TabList className="mt-4 flex justify-center md:justify-start">
+            <Tab>Overview</Tab>
+            <Tab>Quarterfinals</Tab>
+            <Tab>Semifinals</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Overview />
+            </TabPanel>
+
+            <TabPanel>
+              <Quarterfinals />
+            </TabPanel>
+
+            <TabPanel>
+              <Semifinals />
+            </TabPanel>
+
+          </TabPanels>
+        </TabGroup>
         <Footer />
       </div>
       <Toaster />
