f6a6afea8e9c4012901880b19159d3a86aee0fb5
Nathnael Dereje <nathandere1357@gmail.com>
2024-08-10 19:35:43 +0300

    Add asset images

---
+++ src/pages/semifinals.tsx
@@ -0,0 +1,11 @@
+
+export default function Semifinals() {
+    return (
+        <div className="container flex flex-col gap-0">
+            <h1>Semifinals</h1>
+            <div className="flex border justify-center align-middle">
+                <iframe src="https://lottie.host/embed/c568b578-5b2e-47fa-b279-56640a0ab963/6oMjNMIIeQ.json" className="h-[50vh]"></iframe>
+            </div>
+        </div>
+    );
+}
