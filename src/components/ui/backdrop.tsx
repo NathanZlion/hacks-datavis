4ad39a687f9be9bea42379518cf873acd2884144
Nathnael Dereje <nathandere1357@gmail.com>
2024-08-11 15:50:01 +0300

    Add Accordion, Backdrop and Badge components

--- src/components/ui/backdrop.tsx
+++ src/components/ui/backdrop.tsx
@@ -10,7 +10,8 @@ const BackDrop = ({
     return (
         <motion.div
             key={"backdrop"}
-            className="fixed inset-0 top-0 left-0 h-screen w-full bg-[#00000099] z-50 flex justify-center align-middle"
+            className="fixed inset-0 top-0 left-0 h-screen w-full bg-[#00000099] dark:bg-[#43434399] z-50 flex justify-center align-middle backdrop-blur-[2px]"
+            id="backdrop"
             onClick={onClick}
             initial={{opacity: 0}}
             animate={{opacity: 1}}
